6d0b994698f1c5ff16e8a677ba0ee6b5d419fe86
gabrielhgcamargo <gacamargos4@gmail.com>
2022-01-15 17:27:45 -0300

    first commit

---
+++ src/gabrielhgcamargo/github/io/Entendendo_metodos_java/Main.java
@@ -0,0 +1,8 @@
+package gabrielhgcamargo.github.io.Entendendo_metodos_java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
