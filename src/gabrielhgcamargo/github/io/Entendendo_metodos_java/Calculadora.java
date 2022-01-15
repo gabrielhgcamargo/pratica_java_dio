68c8f77abd9f42771fd120b0adbae309ae56231b
gabrielhgcamargo <gacamargos4@gmail.com>
2022-01-15 17:50:10 -0300

    Correcao Calculadora

--- src/gabrielhgcamargo/github/io/Entendendo_metodos_java/Calculadora.java
+++ src/gabrielhgcamargo/github/io/Entendendo_metodos_java/Calculadora.java
@@ -1,16 +1,6 @@
 package gabrielhgcamargo.github.io.Entendendo_metodos_java;
 
 public class Calculadora {
-    private Integer valor1;
-    private Integer valor2;
-
-    public Calculadora(Integer valor1, Integer valor2) {
-        this.valor1 = valor1;
-        this.valor2 = valor2;
-    }
-
-    public Calculadora() {
-    }
 
     public  Integer soma(Integer valor1, Integer valor2){
         return valor1 + valor2;
